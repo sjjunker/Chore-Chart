1d7676fa0f09feabce4d56ceb1c8b79374d7148f
Sandi Junker <sandi.junker@outlook.com>
2024-10-06 14:10:11 -0500

    Initial Commit

---
+++ Chore Chart/Chore_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chore_ChartApp.swift
+//  Chore Chart
+//
+//  Created by Sandi Junker on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Chore_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chore ChartTests/Chore_ChartTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chore_ChartTests.swift
+//  Chore ChartTests
+//
+//  Created by Sandi Junker on 10/6/24.
+//
+
+import Testing
+@testable import Chore_Chart
+
+struct Chore_ChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
